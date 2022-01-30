a19c6a95a8d5eaec838d339198532d9c2d46e911
NazarenkoNad <nazarenkonad84@mail.ru>
2022-01-30 20:42:40 +0600

    Дом работа 3

---
+++ src/homework3/HomeWork3.java
@@ -0,0 +1,26 @@
+package homework3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+
+        Apple apple1 = new Apple();
+        Apple apple2 = new Apple();
+        Apple apple3 = new Apple();
+
+        Orange orange1 = new Orange();
+        Orange orange2 = new Orange();
+
+        Box<Apple> box1 = new Box<Apple>(apple1, apple2, apple3);
+        Box<Orange> box2 = new Box<Orange>(orange1, orange2);
+
+        System.out.println(box1.compare(box2));
+
+        Box<Orange> box3 = new Box<Orange>();
+        box2.transfer(box3);
+    }
+    }
+
+
